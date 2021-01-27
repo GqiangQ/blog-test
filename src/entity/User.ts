7db98488008d018b24878ee1c1b5892c7586570d
GQiangQ <740407980@qq.com>
2021-01-27 23:53:15 +0800

    让scr文件夹下面的东西热跟新

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -2,17 +2,27 @@ import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
 @Entity()
 export class User {
-
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn('increment')
     id: number;
 
-    @Column()
-    firstName: string;
+    @Column('varchar')
+    username: string;
 
-    @Column()
-    lastName: string;
+    @Column('varchar')
+    password: string;
 
-    @Column()
+    @Column('number')
     age: number;
+
+    @Column('number')
+    phoneNumber: number
+
+    @Column('boolean')
+    six: boolean
+
+    @Column({type:"timestamp"})
+    createAt: Date
+    @Column({type: "timestamp"})
+    updateAt: Date
 
 }
